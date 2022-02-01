f0941339b616ebc27ae9056221bb5718950d7f79
Jonathan Soto <97468378+JonaSoto@users.noreply.github.com>
2022-02-01 12:04:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = "Jonathan";
+var lastName = "Soto";
+var age = 19;
+
+console.log("I am having a good time in the class.");
+
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old.");
